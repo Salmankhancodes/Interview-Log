72c079804488b65d1f4b22d97796c7b4b1aa9de2
Salmankhancodes <117salmankhan@gmail.com>
2021-08-08 16:05:18 +0530

    Register API validation

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const connect = require('./config/db')
+
+const bodyParser = require('body-parser')
 require('dotenv').config()
 const app = express()
-
+const router = require('./routes/userRoutes')
 // connect mongodb database
 connect()
-app.get('/', (req, res) => {
-  res.send('Hello mern blog')
-})
+app.use(bodyParser.json())
+app.use('/', router)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log('Your app is running')
---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const app = require('express')
+const router = app.Router()
+const {
+  register,
+  registerValiations,
+} = require('../controllers/userController')
+router.post('/register', registerValiations, register)
+module.exports = router
