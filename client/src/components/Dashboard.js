44dd75ed664aefc299785929aa8bd09a16f9df77
Salmankhancodes <117salmankhan@gmail.com>
2021-08-10 20:44:57 +0530

    Store post in mongodb

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,20 @@
 import { Helmet } from 'react-helmet'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import toast, { Toaster } from 'react-hot-toast'
+import { REDIRECT_FALSE, REMOVE_MESSAGE } from '../store/types/PostTypes'
 const Dashboard = () => {
+  const { redirect, message } = useSelector((state) => state.PostReducer)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (redirect) {
+      dispatch({ type: REDIRECT_FALSE })
+    }
+    if (message) {
+      toast.success(message)
+      dispatch({ type: REMOVE_MESSAGE })
+    }
+  }, [])
   return (
     <>
       <Helmet>
@@ -7,6 +22,15 @@ const Dashboard = () => {
         <meta name='description' content='User Dashboard' />
       </Helmet>
       <h1>Dashboard</h1>
+      <Toaster
+        position='top-center'
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            fontSize: '14px',
+          },
+        }}
+      />
     </>
   )
 }
