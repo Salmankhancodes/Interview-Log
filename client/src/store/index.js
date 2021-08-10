3e239d0124d2a39f712aa34f0fd8da1f2cc49406
Salmankhancodes <117salmankhan@gmail.com>
2021-08-10 20:20:19 +0530

    Post backend validation

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,12 +2,15 @@ import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import AuthReducer from './reducers/AuthReducer'
+import PostReducer from './reducers/PostReducer'
 const rootReducers = combineReducers({
   AuthReducer,
+  PostReducer,
 })
 const middlewares = [thunkMiddleware]
 const Store = createStore(
   rootReducers,
+
   composeWithDevTools(applyMiddleware(...middlewares))
 )
 export default Store
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const connect = require('./config/db')
 const router = require('./routes/userRoutes')
+const postRoutes = require('./routes/postRoutes')
 require('dotenv').config()
 const app = express()
 // connect mongodb database
 connect()
 app.use(bodyParser.json())
 app.use('/', router)
+app.use('/', postRoutes)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log('Your app is running')
